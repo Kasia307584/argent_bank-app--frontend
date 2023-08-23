0949f5a4ebd1d8205b9629088db5612dd29713f6
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-08-23 12:12:19 +0200

    project structure launch

---
+++ src/router/Router.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+export default function Router() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Layout />}>
+        <Route path="user/:userId" element={<Dashboard />} />
+      </Route>
+      <Route path="*" element={<Error />} /> */}
+    </Routes>
+  );
+}
